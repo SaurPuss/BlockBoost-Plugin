919baed345527348bd929b06379ed9b4b17d6033
SaurPuss <saurlemons@gmail.com>
2019-12-09 11:17:46 +0100

    cleanup

--- BlockBoost_1_13/src/main/java/me/saurpuss/blockboost/util/BB.java
+++ BlockBoost_1_13/src/main/java/me/saurpuss/blockboost/util/BB.java
@@ -2,7 +2,7 @@ package me.saurpuss.blockboost.util;
 
 public enum BB {
     BOUNCE("bounceBlocks.yml", "bounce"),
-    SPEED("speedBlocks.yml", null),
+    SPEED("speedBlocks.yml", null), // should always be listed before subcategories
     SPEED_ADDITION("speedBlocks.yml", "addition"),
     SPEED_MULTIPLIER("speedBlocks.yml", "multiplier"),
     POTION("potionBlocks.yml", "potion");
